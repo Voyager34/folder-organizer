8850c2e87032e3e3d518b8a7edc16c85ea392b53
zorg <zorgx@aol.in>
2021-07-27 16:38:01 +0530

    number of files in current directory added

--- listdir.py
+++ listdir.py
@@ -1,12 +1,20 @@
-# this is a comment
+
 import os
 import sys
 
 
 user_folder = sys.argv[1]
-print(f"Gonna organize this folder : {user_folder}")
+print(f"\n Gonna organize this folder : \n {user_folder} \n")
 
 list_files = os.listdir(user_folder)
-print(f" Here.. All files in the directory: \n  {list_files}")
+print(f" Here.. All files in the directory: \n \n {list_files} \n ")
 
+##Print no of files and folders in directory and commit push 
+number_files = len(list_files)
+print(f"Number of files in current directory : \n {number_files} \n")
+
+##WTBD: Do not include hidden files in number_files
+
+
+##WTBD 2 : New program : if directory is passed , make a new directory
 
