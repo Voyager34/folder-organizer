6ee9d33ab03a8130d465bbdf3a0242b4cd6594fd
zorg <zorgx@aol.in>
2021-07-29 19:18:39 +0530

    create help menu with quit and added ambiguous input

--- makedir.py
+++ makedir.py
@@ -3,16 +3,20 @@ import sys
 
 create_folder = sys.argv[1]
 
-answer = input(f"\nare you sure of creating a new directory named {create_folder},   Y  or  N ? ")
-if answer == 'Y': # or 'yes' or 'Yes' or 'YES':
-      print (f"\n new directory created : \n {create_folder}")
-      new_folder = os.mkdir(create_folder)
-else: 
-     print ("\n No folder created! Always nice to be sure :D")
+if create_folder == "--help":
+    print("HERE IS HELP MENU BELOW:: HAVE FUN LOSER!!\n")
+    print(" 1. PASS DIRECTORY AS 1ST ARGUMENT \n 2. SIT BACK AND RELAX \n ")
+    quit()
 
 
+answer = input(
+    f"Are you sure of creating a new directory named {create_folder},  Y  or  N ?"
+)
 
-     
-##print(f"\n Are you sure of creating a directrory named {create_folder}/n")
-##print("Y  or  N  ??")
 
+if answer[0].lower() == "y":
+    print(f"\n New directory created : {create_folder}")
+    new_folder = os.mkdir(create_folder)
+
+else:
+    print("\nNo folder created! Always nice to be sure :D")
