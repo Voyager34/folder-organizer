c1da5ec487ee3311dd9d5f6afbe92d2f2c2996ed
zorg <zorgx@aol.in>
2021-08-02 22:45:14 +0530

    UPDATED PROGRAM TO EXCLUDE HIDDEN FILES

--- listdir.py
+++ listdir.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 
@@ -9,12 +8,24 @@ print(f"\n Gonna organize this folder : \n {user_folder} \n")
 list_files = os.listdir(user_folder)
 print(f" Here.. All files in the directory: \n \n {list_files} \n ")
 
-##Print no of files and folders in directory and commit push 
+##Print no of files and folders in directory and commit push
 number_files = len(list_files)
 print(f"Number of files in current directory : \n {number_files} \n")
 
 ##WTBD: Do not include hidden files in number_files
 
+# HIDDEN FILE LIST
+hidden_file_list = []
+for file in list_files:
+    if file.startswith("."):
+        hidden_file_list.append(file)
 
-##WTBD 2 : New program : if directory is passed , make a new directory
+print("HERE IS LIST OF HIDDEN FILES: \n")
+print(hidden_file_list)
+print(f"\n NO OF HIDDEN FILES : \n {len(hidden_file_list)}")
+
+# NEW LIST WITHOUT HIDDEN FILES (USED LIST COMPREHENSION OR SOME S***)
+updated_list = [x for x in list_files if x not in hidden_file_list]
+print(f"\n UPDATED LIST WITHOUT HIDDEN FILES : \n {updated_list}")
+print(f"\n NO OF FILES IN UPDATED LIST : \n {len(updated_list)}")
 
